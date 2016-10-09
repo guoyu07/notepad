467eca68475ca76c374178c80ba563298dbfdc26
Raghuvaran Chowda on HP <rchowda@g.clemson.edu>
2016-10-09 00:08:31 -0400

    Working DAO

---
+++ pp2/model/PlayerDao.cpp
@@ -0,0 +1,69 @@
+//
+// Created by rchowda on 10/8/2016.
+//
+
+#include <fstream>
+#include <iostream>
+#include "PlayerDao.h"
+
+bool PlayerDao::createNewPlayer(Player &player) {
+    if(isPresent(player.getName())) return false;
+    if(!outFile.is_open()){outFile.open(fileName,std::ios_base::app);}
+    outFile << player.getName() << std::endl;
+    outFile << player.getSymbol() << std::endl;
+    outFile.close();
+    return true;
+}
+
+Player PlayerDao::getPlayer(std::string name) {
+    if(!inFile.is_open()){inFile.open(fileName);}
+    std::string tmp_str;
+    if(!inFile.eof()) inFile >> tmp_str;
+    while(!inFile.eof()) {
+        if (tmp_str == name) {
+            inFile >> tmp_str;
+            return Player(name, tmp_str);
+        }
+        inFile >> tmp_str;
+    }
+    inFile.close();
+    return Player();
+}
+
+std::list<Player> PlayerDao::getAllPlayers() {
+    std::list<Player> playersList;
+    if(!inFile.is_open()){inFile.open(fileName);}
+    std::string tmp_name_str, tmp_sym_str;
+    if(!inFile.eof()) {
+        inFile >> tmp_name_str;
+        inFile >> tmp_sym_str;
+    }
+    while(!inFile.eof()){
+        playersList.push_back(Player(tmp_name_str,tmp_sym_str));
+        inFile >> tmp_name_str;
+        inFile >> tmp_sym_str;
+    }
+    inFile.close();
+    return playersList;
+}
+
+bool PlayerDao::isPresent(const std::string& name) {
+    if(!inFile.is_open()){inFile.open(fileName);}
+    std::string tmp_str;
+    if(!inFile.eof()) inFile >> tmp_str;
+    while(!inFile.eof()){
+        if(tmp_str == name){
+            inFile.close();
+            return true;
+       }
+        inFile >> tmp_str;
+    }
+    return false;
+}
+
+//int main(){
+//    PlayerDao playerDao;
+//    Player one("Raghuvaran",'p');
+//
+//    std::cout << playerDao.createNewPlayer(one) << std::endl;
+//}
