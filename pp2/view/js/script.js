c6b458d4539b4bf6f28aed3e8cf93fd0920cfa23
Raghuvaran Chowda <rchowda@clemson.edu>
2016-10-13 17:36:21 -0400

    working; may be required to format

--- pp2/view/js/script.js
+++ pp2/view/js/script.js
@@ -3,7 +3,7 @@ var cgiPath = "cgi-bin/ttt.cgi";
 
 var playerList = {};
 
-var cursor = "000000000";
+var boardCursor = "000000000";
 
 var tmp_player = {
     1:{}, 2:{}
@@ -11,11 +11,9 @@ var tmp_player = {
 };
 var player = {
     1:{
-        "name":"RV",
-        "marker":"x"
+
     }, 2:{
-        "name":"SW",
-        "marker":"o"
+
     }
 
 };
@@ -27,6 +25,10 @@ var btn1 = document.getElementById('btn1');
 var btn2 = document.getElementById('btn2');
 
 var are_players_set = false;
+var lock_cell = false;
+
+var table = document.getElementById('table');
+table.setAttribute('class','inactive');
 
 var cells = Array.from(document.getElementById('table').children);
 var playResponse = {};
@@ -40,14 +42,18 @@ getAllPlayers();
 //Event listeners for board
 cells.forEach(function (cell) {
 
-	var handler = function(){
-        if(are_players_set) {
+    var handler = function(){
+        if(are_players_set && !lock_cell) {
+            lock_cell = true;
             informPlaying(cell.id);
             togglePlayer();
+            lock_cell = false;
+            cell.removeEventListener('click',handler);
         }
 
-	cell.removeEventListener('click',handler);
-	};
+
+
+    };
 
     cell.addEventListener('click',handler);
 });
@@ -82,13 +88,13 @@ function togglePlayer(){
 }
 
 
-function display(id,mark){
+function display(id,mark,curr_player){
 var cell = document.getElementById(id);
     cell.innerHTML = mark;
-    if(player_num == 1) cell.setAttribute('style','color:red');
+    if(curr_player == 1) cell.setAttribute('style','color:red');
     else cell.setAttribute('style','color:blue');
     playResponse= {
-        "player": player_num,
+        "player": curr_player,
         "position": id
     };
     console.log(JSON.stringify(playResponse));
@@ -131,7 +137,7 @@ function getAllPlayers(){
 //done
 function updatePlayerByDD(dd, playerId, p_name, p_marker,p_form){
     if(dd.selectedIndex == 0) return;
-    console.log(playerList[dd.selectedIndex]);
+  //  console.log(playerList[dd.selectedIndex-1]);
     tmp_player[playerId].name = playerList[dd.selectedIndex-1].name;
     tmp_player[playerId].marker = playerList[dd.selectedIndex-1].marker;
     validatePlayers();
@@ -158,83 +164,108 @@ function validatePlayers(){
     var xhttp = new XMLHttpRequest();
 
     xhttp.open("POST",cgiPath,true);
-    var outJson ={};
-    outJson.identifier = "V";
+    var outJson ={
+        identifier:"",player1_name:null,player1_marker:null,player2_name:"",player2_marker:""
+    };
+    outJson.identifier = "X";
+
+    if(tmp_player[1].name == undefined || tmp_player[1].marker == undefined) outJson.identifier = "Y";
     outJson.player1_name = tmp_player[1].name;
     outJson.player1_marker = tmp_player[1].marker;
     outJson.player2_name = tmp_player[2].name;
     outJson.player2_marker = tmp_player[2].marker;
+
     xhttp.send(JSON.stringify(outJson));
 
     xhttp.onreadystatechange = function () {
-        var inJson = JSON.parse(this.responseText);
-        var players = inJson.players;
-        if(players[0].id > 0){
-            document.getElementById('playerSel1').setAttribute('class','done');
-            document.getElementById('p1_n').innerHTML = players[0].name;
-            document.getElementById('p1_m').innerHTML = players[0].marker;
-            player[1].name = players[0].name;
-            player[1].marker= players[0].marker;
-        }if(players[1].id > 0){
-            document.getElementById('playerSel2').setAttribute('class','done');
-            document.getElementById('p2_n').innerHTML = players[1].name;
-            document.getElementById('p2_m').innerHTML = players[1].marker;
-            player[2].name = players[1].name;
-            player[2].marker= players[1].marker;
-        }
-        if(players[0].id >0 && players[1].id > 0){
-            are_players_set = true;
+        if (this.readyState == 4 && this.status == 200) {
+            var inJson = JSON.parse(this.responseText);
+            var players = inJson.players;
+            if (players[0].id > 0) {
+                document.getElementById('playerSel1').setAttribute('class', 'done');
+                document.getElementById('p1_n').innerHTML = players[0].name;
+                document.getElementById('p1_m').innerHTML = players[0].marker;
+                player[1].name = players[0].name;
+                player[1].marker = players[0].marker;
+            }
+            if (players[1].id > 0) {
+                document.getElementById('playerSel2').setAttribute('class', 'done');
+                document.getElementById('p2_n').innerHTML = players[1].name;
+                document.getElementById('p2_m').innerHTML = players[1].marker;
+                player[2].name = players[1].name;
+                player[2].marker = players[1].marker;
+            }
+            if (players[0].id > 0 && players[1].id > 0) {
+                are_players_set = true;
+                table.className = "active";
+            }
         }
     }
-
 
 }
 
 function informPlaying(id) {
+    if(player_num == 1) boardCursor = boardCursor.substring(0,id-1) + '1' + boardCursor.substring(id,9);
+        else boardCursor = boardCursor.substring(0,id-1) + '2' + boardCursor.substring(id,9);
+    var xhttp2 = new XMLHttpRequest();
+
+    xhttp2.open("POST",cgiPath,true);
     var shootJson = {};
     shootJson.identifier = "P";
     shootJson.player1_name = player[1].name;
     shootJson.player1_marker = player[1].marker;
     shootJson.player2_name = player[2].name;
     shootJson.player2_marker = player[2].marker;
-    shootJson.cursor = cursor;
-    var xhttp = new XMLHttpRequest();
+    shootJson.cursor = boardCursor;
+    console.log(JSON.stringify(shootJson));
 
-    xhttp.open("POST",cgiPath,true);
-    xhttp.send(JSON.stringify(shootJson));
+    xhttp2.send(JSON.stringify(shootJson));
 
-    xhttp.onreadystatechange = function () {
-        var inJson = JSON.parse(this.responseText);
-        displayBoard(inJson.cursor);
-        if(inJson.winner > 0 && inJson.winner <4){
-            declareWinner(inJson.winner);
-            are_players_set = false;
+    xhttp2.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+            var inJson = JSON.parse(this.responseText);
+
+            boardCursor = inJson.cursor;
+            displayBoard();
+            if (inJson.winner > 0 && inJson.winner < 4) {
+                declareWinner(inJson.winner);
+                are_players_set = false;
+                table.className = "inactive";
+            }
+
         }
 
-    };
+    }
 
 }
 
 
-function displayBoard(cursor) {
-    var tmp = player_num;
-    player_num = 1;
+function displayBoard() {
+    var curr_player = player_num;
     for(var i=0; i <9; i++){
-        display(i,cursor[i]); //id, marker
-        togglePlayer();
-    }
-    player_num = tmp;
+        if(boardCursor[i]==1) {
+            display(i+1,player[1].marker,1);
+        } //id, marker
+        else if(boardCursor[i]==2) {
+            display(i+1,player[2].marker,2);
+        } //id, marker
+        else if(boardCursor[i]==0){
+
+        }else console.log('error in displayBoard:'+boardCursor[i]);
+
+    }        console.log('displayBoard: '+boardCursor);
+
 }
 
 
 function declareWinner(winner) {
     var result = document.getElementById('winner');
-    var resultDiv = document.getElementsByClassName('result');
+    var resultDiv = document.getElementById('result');
     switch(winner){
         case 1: result.innerHTML = player[1].name; break;
         case 2: result.innerHTML = player[2].name; break;
         case 3: result.innerHTML = 'Tie'; break;
     }
-    resultDiv.classList.remove('done');
+    resultDiv.className = "";
 }
-
