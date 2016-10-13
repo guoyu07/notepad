fb6fc027549aa8a65ac3ac9ef8684373194ac232
Raghuvaran Chowda <rchowda@clemson.edu>
2016-10-13 17:37:18 -0400

    changed variable names and few impl modifications

--- pp2/main.cpp
+++ pp2/main.cpp
@@ -10,17 +10,17 @@ int main() {
     //receive json into string
     std::stringstream inBuff;
 
-   inBuff << std::cin.rdbuf();
+//   std::getline(std::cin.rdbuf(),inBuff );
 
     std::string inJsonRequest = inBuff.str();
-
+//
 //    std::ofstream file("jsonOut");
 //    file << inJsonRequest;
 //    file.close();
 //
 //    std::cout << inJsonRequest ; return 0;
 
-//    std::cin >> inJsonRequest;
+    std::getline(std::cin,inJsonRequest);
 
     TTTController tttController;
     //check string for identifiers
@@ -53,7 +53,7 @@ int main() {
 
             tttController.startNewGame();
 
-            //parse cursor into array
+            //parse boardCursor into array
             tttController.partParseJson(inJsonRequest, cursor);
             // setSelection accordingly
             for (int i = 0; i < 9; i++) {
@@ -67,19 +67,27 @@ int main() {
             else std::cout << cursor << ",\"winner\":" << tttController.determineWinner() << "}";
             break;
         }
-        case 'V':{
+        case 'X': {
             std::string player_name, player_marker;
-            tttController.partParseJson(inJsonRequest,player_name);
-            tttController.partParseJson(inJsonRequest,player_marker);
-            tttController.createPlayer(player_name,player_marker,1);
             tttController.partParseJson(inJsonRequest, player_name);
             tttController.partParseJson(inJsonRequest, player_marker);
-            tttController.createPlayer(player_name, player_marker, 2);
+            if (player_name == "" || player_marker == "") {}
+            else tttController.createPlayer(player_name, player_marker, 1);
+        }
+        case 'Y':{
+            std::string player_name, player_marker;
+            tttController.partParseJson(inJsonRequest, player_name);
+            tttController.partParseJson(inJsonRequest, player_marker);
+            if(player_name =="" || player_marker =="") {}
+                else tttController.createPlayer(player_name, player_marker, 2);
+
+
+            tttController.startNewGame();
             std::string players;
 
             players += "{\"players\":[";
-            players += "{\"name\":\"" + tttController.player1.getName() + "\",\"marker\":\"" + tttController.player1.getSymbol() + "\",\"id\":"; players+= tttController.player1.getId() + "},";
-            players += "{\"name\":\"" + tttController.player2.getName() + "\",\"marker\":\"" + tttController.player2.getSymbol() + "\",\"id\":"; players+= tttController.player2.getId() + "}";
+            players += "{\"name\":\"" + tttController.player1.getName() + "\",\"marker\":\"" + tttController.player1.getSymbol() + "\",\"id\":" +  std::to_string(tttController.player1.getId()) + "},";
+            players += "{\"name\":\"" + tttController.player2.getName() + "\",\"marker\":\"" + tttController.player2.getSymbol() + "\",\"id\":" + std::to_string(tttController.player2.getId()) + "}";
             players += "]}";
             std::cout << players;
             break;
