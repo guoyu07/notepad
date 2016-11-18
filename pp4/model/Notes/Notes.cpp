7b08e54041e4cc737bdaa7d31c9283fe86cc95c2
Raghuvaran Chowda on HP <rchowda@g.clemson.edu>
2016-11-18 00:40:32 -0500

    Notes model incomplete

---
+++ pp4/model/Notes/Notes.cpp
@@ -0,0 +1,7 @@
+//
+// Created by rchowda on 11/18/2016.
+//
+
+#include "Notes.h"
+
+Notes::Notes(int noteId, const std::string &title, const std::string &body, const User &owner) : noteId(noteId), title(title), body(body), owner(owner) {}
---
+++ pp4/model/Notes/Notes.h
@@ -0,0 +1,22 @@
+//
+// Created by rchowda on 11/18/2016.
+//
+
+#ifndef PP4_NOTES_H
+#define PP4_NOTES_H
+
+
+#include <string>
+#include "../Users/User/User.h"
+
+class Notes {
+protected:
+    int noteId;
+    std::string title, body, lastModified;
+    User owner;
+public:
+    Notes(int noteId, const std::string &title, const std::string &body, const User &owner);
+};
+
+
+#endif //PP4_NOTES_H
