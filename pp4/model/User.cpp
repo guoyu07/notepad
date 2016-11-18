6ae71f14042073198856f561c683bc035b61625e
Raghuvaran Chowda on HP <rchowda@g.clemson.edu>
2016-11-17 23:46:22 -0500

    Initial commit

---
+++ pp4/model/User.cpp
@@ -0,0 +1,5 @@
+//
+// Created by rchowda on 11/17/2016.
+//
+
+#include "User.h"
---
+++ pp4/model/User.h
@@ -0,0 +1,14 @@
+//
+// Created by rchowda on 11/17/2016.
+//
+
+#ifndef PP4_USER_H
+#define PP4_USER_H
+
+
+class User {
+
+};
+
+
+#endif //PP4_USER_H
