17c2bc69d5b14fd8d2739130dceec5e7f4f72dbd
Raghuvaran Chowda on HP <rchowda@g.clemson.edu>
2016-11-19 12:42:06 -0500

    Expecting to work code

--- pp4/view/js/script.js
+++ pp4/view/js/script.js
@@ -1,12 +1,18 @@
 var cgiPath = "cgi-bin/notes.cgi";
-
+var create=0, get=1, put=2, del=3;
 var notes = {
   0:{
       "noteId" : -1,
-      "noteTitle" : "",
-      "noteBody" : "",
-      "lastModifiedOn" : ""
-  }
+      "noteTitle" : "dummy1",
+      "noteBody" : "dummy11",
+      "lastModifiedOn" : "never1"
+  },
+    1:{
+        "noteId" : -2,
+        "noteTitle" : "dummy2",
+        "noteBody" : "dummy21",
+        "lastModifiedOn" : "never2"
+    }
 };
 
 checkCookie();
@@ -57,36 +63,141 @@ function authenticate(userName, password){
 //check cookie
 
 //******//Assume user is authenticated
-
+var userName;
 /*
 * Get all notes list (titles)
 * should have note Id, title, body, last modified, etc.
 * */
+function getNotes() {
+    var xhttp = new XMLHttpRequest();
+    var outJson = {};
+    outJson.identifier = "g";
+    outJson.userName = userName;
 
+    xhttp.open("POST", cgiPath, true);
+    xhttp.send(JSON.stringify(outJson));
+
+    xhttp.onreadystatechange = function () {
+        if(this.readyState == 4 && this.status == 200){
+            var inJson = JSON.parse(this.responseText);
+            notes = inJson.notes;
+            updateNotesDisplay();
+
+        }
+    };
+
+}
+
+function updateNotesDisplay() {
+    $('#notesContainer').find('a')
+        .remove()
+        .end();
+    for(var i=0; i=notes.length; i++){
+        $('#notesContainer').find('a').append('<a href ="#"  class="list-group-item list-group-item-action" onClick="displayNoteModal('+i+');">'+notes[i].noteTitle+'</a>')
+    }
+
+}
+
+function displayNoteModal(noteIndex){
+
+    $("#noteTitle").val(notes[noteIndex].noteTitle);
+    $("#noteBody").val(notes[noteIndex].noteBody);
+
+    var createHandler = function () {
+        updateNote(noteIndex,create);
+        $("#myModal").modal('hide');
+        $("#deleteNote").show();
+        $("#updateNote").unbind('click', createHandler);
+    };
+
+    var	updateHandler = function(){
+        updateNote(noteIndex, put);
+        $("#myModal").modal('hide');
+        $("#updateNote").unbind('click', updateHandler);
+        $("#deleteNote").unbind('click', deleteHandler);
+    };
+    var	deleteHandler = function(){
+        updateNote(noteIndex, del);
+        $("#myModal").modal('hide');
+        $("#updateNote").unbind('click', updateHandler);
+        $("#deleteNote").unbind('click', deleteHandler);
+    };
+
+    $("#myModal").modal('show');
+
+    if(noteIndex == -1){ //New note
+        $("#deleteNote").hide();
+        $("#updateNote").on('click', createHandler);
+    }else{              //update or delete
+        $("#updateNote").on('click', updateHandler);
+        $("#deleteNote").on('click', deleteHandler);
+    }
+
+
+}
 //On update
 /*
-* Load notes into modal
-* bind onSave btn to update local variable and send update request*/
-/***call getAllNotes***/
-
+ * Load notes into modal
+ * bind onSave btn to update local variable and send update request*/
 
 //On delete
 /*
-* delete local variable and send delete request*/
+ * delete local variable and send delete request*/
 
 
-/***call getAllNotes***/
+function updateNote(noteIndex, mode){
+    var nTitle = $("#noteTitle").val();
+    var nBody = $("#noteBody").val();
+    var outJson = {};
 
+    switch(mode){
+        case create: outJson.identifier = "c"; break;
+        case get:    outJson.identifier = "g"; break;
+        case put:    outJson.identifier = "p"; break;
+        case del:    outJson.identifier = "d"; break;
+    }
+    outJson.note.noteId = noteIndex;
+    outJson.note.noteTitle = nTitle;
+    outJson.note.noteBody  = nBody;
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", cgiPath, true);
+    xhttp.send(JSON.stringify(outJson));
+
+    xhttp.onreadystatechange = function () {
+        if(this.readyState == 4 && this.status == 200) {
+            var inJson = JSON.parse(this.responseText);
+            if(Boolean(inJson.success))
+                console.log("update successful");
+            else
+                console.log("update failed");
+            /***call getAllNotes***/
+            getNotes();
+        }
+    };
+
+}
 
 //On create
 /*
 * create new note locally and send create request*/
 
+
 /***call getAllNotes***/
+
+
+/***Admin****/
+//OnChildCreate
+
+
+
+//OnChildDelete
 
 
 //On logout
 /**remove cookie**/
+setCookie("uid",userName,-1);
+checkCookie();
 
 
 
@@ -117,8 +228,7 @@ function authenticate(userName, password){
 
 
 
-
-/******************Cookies*****************/
+/******************#myModal*****************/
 function centerModal() {
     $(this).css('display', 'block');
     var $dialog = $(this).find(".modal-dialog");
@@ -135,22 +245,8 @@ $('.modal').on('show.bs.modal', centerModal);
 $(window).on("resize", function () {
     $('.modal:visible').each(centerModal);
 });
-function updateNote(noteId){
-    $("#myModal").modal('show');
-    $("#noteTitle").val(titleData);
-    var	handler = function(){
-    updateData(noteId);
-    $("#myModal").modal('hide');
-	$("#saveChanges").unbind('click', handler);
-};
 
-
-$("#saveChanges").on('click', handler);
-}
-
-function updateData(noteId){
-    titleData = $("#noteTitle").val();
-}
+// $("#saveChanges").on('click', handler);
 
 
 
@@ -161,7 +257,7 @@ function updateData(noteId){
 
 
 
-
+/******************Cookies*****************/
 function setCookie(cname, cvalue, exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
@@ -187,13 +283,14 @@ function getCookie(cname) {
 function checkCookie() {
     var user = getCookie("username");
     if (user != "") {
-//        user = prompt("Please enter your name:", "");
+       // user = prompt("Please enter your name:", "");
         alert("Welcome again " + user);
+        window.location = '/';
     } else {
         window.location = 'login.html';
-        if (user != "" && user != null) {
-            setCookie("username", user, 365);
-        }
+        // if (user != "" && user != null) {
+        //     setCookie("username", user, 1);
+        // }
     }
 }
 
